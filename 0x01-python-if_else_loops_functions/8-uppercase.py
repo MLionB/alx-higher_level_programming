929ed731d273d4d2ace48cb6708ea1c1cedfd6e6
root <nkbmeaza@gmail.com>
2022-06-29 18:01:53 -0700

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        x = ord(x)
+        if x > 96 and x < 123:
+            x = x - 32
+        print("{:c}".format(x), end='')
+    print()
